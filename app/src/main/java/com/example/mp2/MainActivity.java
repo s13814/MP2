9cf0eb527e3e90ad3412249cc9c5e24b32817a1e
s13814 <s13814@pjwstk.edu.pl>
2019-11-12 04:41:46 +0100

    Końcówka powiadomień

--- app/src/main/java/com/example/mp2/MainActivity.java
+++ app/src/main/java/com/example/mp2/MainActivity.java
@@ -21,4 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         registerReceiver(mbr, new IntentFilter(myIntent));
     }
+
+
 }
--- app/src/main/java/com/example/mp2/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/mp2/MyBroadcastReceiver.java
@@ -3,9 +3,14 @@ package com.example.mp2;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
@@ -13,23 +18,37 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
-    private String channel = "chan1";
     private int id = 0;
     @Override
     public void onReceive(Context context, Intent intent) {
+        String channe1Id = "chan1";
+        String channelName = "myChannel";
+
         String name = intent.getStringExtra("name");
         int price = intent.getIntExtra("price", 0);
 
+        /*Intent listIntent = new Intent();
+        listIntent.setComponent(new ComponentName("com.example.mp1", "com.example.mp1.ProductListActivity"));*/
+
+        Intent listIntent = context.getPackageManager().getLaunchIntentForPackage("com.example.mp1");
+
+        PendingIntent pIntent = PendingIntent.getActivity(context, 0, listIntent, 0);
+
         Toast.makeText(context, "Nazwa: \"" + name + "\" cena: " + price, Toast.LENGTH_LONG).show();
-        NotificationCompat.Builder notif = new NotificationCompat.Builder(context, "chan1")
+        NotificationCompat.Builder notif = new NotificationCompat.Builder(context, channe1Id)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Dodano nowy produkt")
-                .setContentText("Nazwa: " + name + "Cena: " + price)
+                .setContentText("Nazwa: \"" + name + "\" Cena: \"" + price + "\"")
+                .setContentIntent(pIntent)
                 .setAutoCancel(true);
 
-        //if(Buil)
-        //NotificationChannel channel = new NotificationChannel("chan1", channel, NotificationManager.IMPORTANCE_DEFAULT);
-        //channel.setDescription(description);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channe1Id, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Simple notification");
+
+            NotificationManager nm = context.getSystemService(NotificationManager.class);
+            nm.createNotificationChannel(channel);
+        }
 
         NotificationManagerCompat manager = NotificationManagerCompat.from(context);
         manager.notify(id++, notif.build());
